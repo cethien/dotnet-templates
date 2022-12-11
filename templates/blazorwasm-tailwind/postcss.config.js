9c5f84ff23d8c57211af39749918c31a68460129
Cethien <borislaw.sotnikow@gmx.de>
2022-12-11 23:52:28 +0100

    added wasm template

---
+++ templates/blazorwasm-tailwind/postcss.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	plugins: {
+		'tailwindcss/nesting': {},
+		tailwindcss: {},
+		autoprefixer: {},
+		'postcss-preset-env': {
+			features: { 'nesting-rules': false },
+		},
+		cssnano: { preset: 'default' },
+	},
+};
