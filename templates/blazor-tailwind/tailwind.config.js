0334bbe459b6bb41b219f48266a78c2cfe803cc9
Cethien <borislaw.sotnikow@gmx.de>
2022-12-11 22:36:31 +0100

    cleaned up

--- templates/blazor-tailwind/tailwind.config.js
+++ templates/blazor-tailwind/tailwind.config.js
@@ -7,21 +7,7 @@ module.exports = {
 		'./wwwroot/*.{html,js,cshtml,razor}',
 	],
 	theme: {
-		extend: {
-			colors: {
-				primary: '#0369a1',
-				secondary: '#0284c7',
-				hover: '#bae6fd',
-				text: '#000000',
-
-				'dark-bg-primary': '#475569',
-				'dark-bg-secondary': '#1e293b',
-				'dark-primary': '#38bdf8',
-				'dark-secondary': '#0284c7',
-				'dark-hover': '#334155',
-				'dark-text': '#FFFFFF',
-			},
-		},
+		extend: {},
 	},
 	plugins: [],
 	darkMode: 'class',
